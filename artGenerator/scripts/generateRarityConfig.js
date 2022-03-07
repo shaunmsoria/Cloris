d40cfb8c12e779789a6fe7e3b214c60eaa43dbac
Solarisray <shaunmsoria@gmail.com>
2022-03-08 08:31:54 +1100

    generateRarityConfig.js update #1

--- artGenerator/scripts/generateRarityConfig.js
+++ artGenerator/scripts/generateRarityConfig.js
@@ -1,3 +1,6 @@
 const config = require("../settings/config.json");
 const path = require("path");
 const fs = require("fs");
+
+const base = process.cwd();
+const layersBasePath = path.join(base, "/layers");
