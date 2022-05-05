19f4339ca90d61428d3a90658c4794ba54c15edb
Solarisray <solarisray@gmail.com>
2022-05-06 07:44:45 +1000

    introduction of props in react components

--- cloris-project/src/App.js
+++ cloris-project/src/App.js
@@ -1,13 +1,51 @@
 import logo from './logo.svg';
 import { Button } from 'react-bootstrap';
 import './App.css';
-// import {walletUser} from './helpers';
+import {} from './helpers';
+import React, { useCallback } from "react";
+
+import { ethers } from "ethers"; // try to use ether.js to connect to metamask and the cloris smart contract
+
+export async function walletUser2() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    return signer;
+};
 
 
 
+function WalletUser3 () {
+  const walletUser = useCallback(async() => {
+    let provider = new ethers.providers.Web3Provider(window.ethereum)
+    await provider.send("eth_requestAccounts", [])
+    let response = provider.getSigner()
+    console.log(`The value of response is ${String(response)}`);
+  }, []);
+  return walletUser;
+}
+
+const clorisAddress = "0x019500Fd3FbD2c5C3030445D253930aC87767139";
+
+const clorisAbi = [
+   "function mintContract (uint tokensToMint) public payable",
+   "function walletOfOwner(address _owner) public view returns(uint256[] memory)",
+   "function getSecondsUntilMinting() public view returns(uint256)",
+   "function blockCurrentTime() public view returns(uint256)",
+   "function withdraw () public payable onlyOwner ",
+   "function tokenURI(uint256 tokenId) public view virtual override returns ",
+   "function setBaseURI (string memory _newBaseURI) public onlyOwner",
+   "function setExtensionURI (string memory _extensionURI) public onlyOwner ",
+   "function setMaxMintAccount (uint _maxMintAccount) public onlyOwner",
+   "function setIsPaused (bool _isPaused) public onlyOwner"
+];
+
+// export const clorisContract = new ethers.Contract(clorisAddress, clorisAbi, walletUser());
 
 function App() {
-  // const signer = walletUser();
+
+  // const clorisContract = new ethers.Contract(clorisAddress, clorisAbi, signer);
+
   return (
     <div className="App">
       <div className="socialMedia">
@@ -18,8 +56,10 @@ function App() {
           <img src={logo} className="App-logo" alt="logo"/>
         </div>
         <div className="timeText">
+            <span>Value of signer is: {WalletUser3}</span>
           <span>Time left before the minting: "timeLeftBeforeMinting"</span>
-          <button>mint</button>
+          <button /*onClick={updateSigner}*/>mint</button>
+          <span>the value of testA is {}</span>
           <span>Time since the minting "timeSinceMinting"</span>
         </div>
 
--- cloris-project/src/helpers.js
+++ cloris-project/src/helpers.js
@@ -1,34 +1,34 @@
-import { ethers } from "ethers"; // try to use ether.js to connect to metamask and the cloris smart contract
+// import { ethers } from "ethers"; // try to use ether.js to connect to metamask and the cloris smart contract
 
-export async function walletUser() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    return signer;
-}
+// export async function walletUser() {
+//     const provider = new ethers.providers.Web3Provider(window.ethereum);
+//     await provider.send("eth_requestAccounts", []);
+//     const signer = provider.getSigner();
+//     return signer;
+// }
 
-const clorisAddress = "0x019500Fd3FbD2c5C3030445D253930aC87767139";
+// const clorisAddress = "0x019500Fd3FbD2c5C3030445D253930aC87767139";
 
-const clorisAbi = [
-   "function mintContract (uint tokensToMint) public payable",
-   "function walletOfOwner(address _owner) public view returns(uint256[] memory)",
-   "function getSecondsUntilMinting() public view returns(uint256)",
-   "function blockCurrentTime() public view returns(uint256)",
-   "function withdraw () public payable onlyOwner ",
-   "function tokenURI(uint256 tokenId) public view virtual override returns ",
-   "function setBaseURI (string memory _newBaseURI) public onlyOwner",
-   "function setExtensionURI (string memory _extensionURI) public onlyOwner ",
-   "function setMaxMintAccount (uint _maxMintAccount) public onlyOwner",
-   "function setIsPaused (bool _isPaused) public onlyOwner"
-]
+// const clorisAbi = [
+//    "function mintContract (uint tokensToMint) public payable",
+//    "function walletOfOwner(address _owner) public view returns(uint256[] memory)",
+//    "function getSecondsUntilMinting() public view returns(uint256)",
+//    "function blockCurrentTime() public view returns(uint256)",
+//    "function withdraw () public payable onlyOwner ",
+//    "function tokenURI(uint256 tokenId) public view virtual override returns ",
+//    "function setBaseURI (string memory _newBaseURI) public onlyOwner",
+//    "function setExtensionURI (string memory _extensionURI) public onlyOwner ",
+//    "function setMaxMintAccount (uint _maxMintAccount) public onlyOwner",
+//    "function setIsPaused (bool _isPaused) public onlyOwner"
+// ]
 
-export const clorisContract = new ethers.Contract(clorisAddress, clorisAbi, walletUser());
+// export const clorisContract = new ethers.Contract(clorisAddress, clorisAbi, walletUser());
 
 // const cloris = clorisContract;
 // const clorisBlockTime = await cloris.blockCurrentTime();
 //  <span>test value: {clorisBlockTime}</span> 
 
 module.exports = {
-    walletUser,
-    clorisContract
+    // walletUser,
+    // clorisContract
 }
