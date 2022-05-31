ea876c99ff3a8a71c33346a1c84d29e3e744fedb
Solarisray <solarisray@gmail.com>
2022-06-01 07:20:40 +1000

    Created Time Calculation Functions in React synced with the smart contract

--- cloris-project/src/App.js
+++ cloris-project/src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
         this.state = { 
             signer: "",
             provider: new ethers.providers.Web3Provider(window.ethereum),
-            clorisAddress: "0x84bdFF44b36cF1aD0dA2D14cbb138f5a8549aE4d",
+            clorisAddress: "0x9c08F81132f054D24D290E3fa7BC94bC9C7e358C",
             clorisAbi: [
                 "function name() view returns (string)",
                 "function symbol() view returns (string)",
@@ -26,61 +26,57 @@ class App extends React.Component {
                 "function isPaused() view returns (bool)",
                 "function mintContract(uint) payable returns ()"
             ],
-            // date: Math.floor(new Date() / 1000),
-            // countDownDate: Math.floor(new Date("May 31, 2022 16:00:00") / 1000),
             date: Math.floor(new Date() / 1000),
-            countDownDate: Math.floor(new Date("May 31, 2022 07:00:00") / 1000),
+            countDownDate: Math.floor(new Date("Jun 1 2022 07:30:00") / 1000),
+            deploymentTime: "",
             timeLeft: ""
         };
 
     }
 
     calculateTimeLeft(){
-
         const oneDay = 60 * 60 * 24;
         const oneHour = 60 * 60;
         const oneMinute = 60;
-        const calTimeLeft = this.state.countDownDate - this.state.date;
-        const daysLeft = calTimeLeft / oneDay;
-        const hoursLeft = (calTimeLeft % oneDay) / oneHour;
-        const minutesLeft = (calTimeLeft % hoursLeft) / oneMinute;
-        const secondsLeft = calTimeLeft % minutesLeft;
-        
-        if(this.state.countDownDate - this.state.date > 0 ){
-            this.setState(
-                {timeLeft: `Time left before deployment: Days Left: ${daysLeft.toFixed(0)} Hours Left: ${hoursLeft.toFixed(0)} Minutes Left: ${minutesLeft.toFixed(0)} Seconds Left: ${secondsLeft.toFixed(0)}`}
-            )
-        } else {
-            this.setState(
-                {timeLeft: `Contract deployed since: ${(daysLeft * (- 1)).toFixed(0)} Days, ${(hoursLeft * (- 1)).toFixed(0)} Hours, ${(minutesLeft * (- 1)).toFixed(0)} Minutes, ${(secondsLeft * (- 1)).toFixed(0)} Seconds`}
-            )
-        }
+        const isFuture = (this.state.countDownDate - this.state.date >= 0) ? true : false;
+        const calTimeLeft = (this.state.countDownDate - this.state.date >= 0) ? this.state.countDownDate - this.state.date : this.state.date - this.state.countDownDate;
+        const daysLeft = Math.floor((calTimeLeft / oneDay));
+        const hoursLeft = Math.floor(((calTimeLeft & oneDay) / oneHour));
+        const minutesLeft = Math.floor(((calTimeLeft % oneDay) % oneHour) / oneMinute);
+        const secondsLeft = Math.floor(((calTimeLeft % oneDay) % oneHour) % oneMinute);
+        this.setState(
+            {timeLeft: `${(isFuture) ? "Time left before minting:" : "Time since the contract was minted:"} ${daysLeft} Days, ${hoursLeft} Hours, ${minutesLeft} Minutes, ${secondsLeft} Seconds`} 
+        )
+    }
+
+    tick(){
+        this.setState({
+            date: Math.floor(new Date() / 1000)
+        });
+        this.calculateTimeLeft();
     }
 
     async componentDidMount(){
-        // const provider = new ethers.providers.Web3Provider(window.ethereum);
-        // await provider.send("eth_requestAccounts", []);
         await this.state.provider.send("eth_requestAccounts", []);
-        const testSigner = this.state.provider.getSigner();
-        this.setState({signer: testSigner});
-        this.calculateTimeLeft();
-        const blockNumber = await this.state.provider.getBlockNumber();
-        // console.log(`The value of blockNumber is ${blockNumber}`);
-        // const balance = await this.state.provider.getBalance("ethers.eth");
-        // console.log(`The value of balance is ${balance}`);
-        // const balanceEther = ethers.utils.formatEther(balance);
-        // console.log(`The value of balanceEther is ${balanceEther}`);
-        // console.log(`The value of balanceWei is ${ethers.utils.parseEther(balanceEther)}`);
-        // const transaction = this.state.signer.sendTransaction({
-        //      to: "0x40953332Fe1c3838b93Ed6071403A7CD54eb9AE1",
-        //      value: ethers.utils.parseEther("1.0")
-        // });
+        const defineSigner = this.state.provider.getSigner();
+               this.setState({signer: defineSigner});
+
         const clorisContract = new ethers.Contract(this.state.clorisAddress, this.state.clorisAbi, this.state.provider);
         const clorisSigner = clorisContract.connect(this.state.signer);
-        console.log(`The value of testSigner is ${testSigner}`);
+
+        this.setState({deploymentTime: `${new Date(await clorisContract.deployedTime() * 1000)}`});
+        this.calculateTimeLeft();
+
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+
+
+        // Testing the front end
+        console.log(`The value of defineSigner is ${defineSigner}`);
         console.log(`the name of the contract is ${await clorisContract.name()}`);
         console.log(`the symbol of the contract is ${await clorisContract.symbol()}`);
-        console.log(`the deployedTime of the contract is ${await clorisContract.deployedTime()}`);
+        console.log(`the deployedTime of the contract is ${this.state.deploymentTime}`);
         console.log(`the maxMintAccount of the contract is ${await clorisContract.maxMintAccount()}`);
         console.log(`the mintCost of the contract is ${await clorisContract.mintCost()}`);
         console.log(`the maxSupply of the contract is ${await clorisContract.maxSupply()}`);
@@ -91,9 +87,22 @@ class App extends React.Component {
         console.log(`the value of date is ${this.state.date}`);
         console.log(`the value of countDownDate is ${this.state.countDownDate}`)
         console.log(`the value of timeLeft is ${this.state.timeLeft}`);
+        console.log(`The value of blockNumber is ${await this.state.provider.getBlockNumber()}`);
+
+        // const balance = await this.state.provider.getBalance("ethers.eth");
+        // console.log(`The value of balance is ${balance}`);
+        // const balanceEther = ethers.utils.formatEther(balance);
+        // console.log(`The value of balanceEther is ${balanceEther}`);
+        // console.log(`The value of balanceWei is ${ethers.utils.parseEther(balanceEther)}`);
+        // const transaction = this.state.signer.sendTransaction({
+        //      to: "0x40953332Fe1c3838b93Ed6071403A7CD54eb9AE1",
+        //      value: ethers.utils.parseEther("1.0")
+        // });
     }
 
-
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
 
 
 
@@ -111,7 +120,7 @@ class App extends React.Component {
                     </div>
                     <div className="status">
                         <span className="timeReference">
-                            Deployment Time is: {this.state.date}
+                            Deployment Time is: {this.state.deploymentTime}
                         </span>
                         <span className="timeReference">
                             {this.state.timeLeft}
